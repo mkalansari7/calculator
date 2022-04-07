f988dda24ff2054509723ee228eccf138d1f29b5
mkalansari7 <mk.alansari7@gmail.com>
2022-04-08 00:39:48 +0300

    Done

--- calculator.py
+++ calculator.py
@@ -1,8 +1,29 @@
 
 def main():
 	#write your code here
-	pass
 	
+	firstNumber = input("Enter the first number:")
+	secondNumber = input("Enter the second number:")
+	
+	if firstNumber.isdigit() and secondNumber.isdigit():
+		operation = input("choose: (+,-,/,*)")
+		if operation == "+":
+			answer = int(firstNumber) + int(secondNumber)
+			print("The Answer is: ", answer)
+		elif operation == "-":
+			answer = int(firstNumber) - int(secondNumber)
+			print("The Answer is: ", answer)
+		elif operation == "/":
+			answer = int(firstNumber) / int(secondNumber)
+			print("The Answer is: ", answer)
+		elif operation == "*":
+			answer = int(firstNumber) * int(secondNumber)
+			print("The Answer is: ", answer)
+		else:
+			print("You should use a valid operation")
+		
+	else:
+		print("You should enter numbers only")
 
 
 
